6c88238344beca4d7f24b677be93f263db5aaaf6
carolinedevine <cdevine@colgate.edu>
2025-04-08 15:36:11 -0400

    reporting in tables

--- Lab11.R
+++ Lab11.R
@@ -81,10 +81,70 @@ dat.long <- dat.fig2g|>
 
 combined.plot <- ggplot(data = dat.long)+
   geom_boxplot(aes(x=Condition,  
-                   y="Fluoresence (% Change)"))+
+                   y=Fluoresence))+
   theme_bw()+                  
   geom_hline(yintercept=0)+                                   
   theme_bw() +                                                
   xlab("Location") +                                    
-  ylab("Fluoresence (% Change)") 
-  
+  ylab("Fluoresence (% Change)") +
+  ggtitle("Dopamine Levels by Distance")
+
+
+################################################################################
+# Question 4
+################################################################################
+library(effectsize)
+
+### Part A: Close Responses ###
+mu0 <- 0
+x <- dat.fig2g$closer_vals
+# TEST OF WHAT THEY DID, REPORTED ONE-SIDED, BUT DID TWO SIDED
+# p-value
+(p.val <- 2*pt(q=-abs(t.stat), df = n-1)) # this is the two tailed version
+# Hedges G
+g <- hedges_g(x = x, mu = mu0, alternative = "greater")
+interpret_hedges_g(g)
+g.list <- as.list(g)
+
+# T-test
+close.stats <- t.test(x=x, mu = mu0, conf.level = 0.95, alternative = "greater") # one tailed t-test
+
+# Report Stats
+close.table <- tibble(
+  t = close.stats$statistic,
+  p = close.stats$p.value,
+  g = g.list$Hedges_g,
+  CI.low = g.list$CI_low,
+  CI.high = g.list$CI_high,
+)
+view(close.table)
+
+
+### Part B: Far Responses ###
+# Hedges G
+x2 <- dat.fig2g$further_vals
+g2 <- hedges_g(x = x2, mu = mu0, alternative = "less")
+interpret_hedges_g(g2)
+g2.list <- as.list(g2)
+
+# T-test
+far.stats <- t.test(x=x2, mu = mu0, conf.level = 0.95, alternative = "less") # one tailed t-test
+
+# Report Stats
+far.table <- tibble(
+  t = far.stats$statistic,
+  p = far.stats$p.value,
+  g = g2.list$Hedges_g,
+  CI.low = g2.list$CI_low,
+  CI.high = g2.list$CI_high,
+)
+view(far.table)
+
+### Part C: Difference ###
+# Hedges G
+x3 <- dat.fig2g$difference
+g3 <- hedges_g(x = x3, mu = mu0, alternative = "two.sided")
+interpret_hedges_g(g3)
+
+# T-test
+dif.stats <- t.test(x=x3, mu = mu0, conf.level = 0.95, alternative = "two.sided") # paired t-test
